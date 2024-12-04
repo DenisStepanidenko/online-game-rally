b4e2334d309a6f0c4e3655e400c3af3eed162dd6
DenisStepanidenko <stepidenko.den@mail.ru>
2024-12-04 21:10:57 +0300

    initial commit

---
+++ src/main/java/DM/ServerRally/ServerRallyApplication.java
@@ -0,0 +1,13 @@
+package DM.ServerRally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerRallyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerRallyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DM/ServerRally/ServerRallyApplicationTests.java
@@ -0,0 +1,13 @@
+package DM.ServerRally;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerRallyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
